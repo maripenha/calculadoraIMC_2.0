85e4a70b3b9d9c145596a0619c2384f105fbfe2f
Mariana Segovia Penha <158625854+maripenha@users.noreply.github.com>
2024-04-16 19:38:26 +0000

    final

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+function CapValores(event){
+    event.preventDefault()
+
+    let dadosUsuario = CapturarValores();
+
+    let imc = Number(calcularImc(dadosUsuario.altura, dadosUsuario.peso));
+
+    let classificacao = ClassificarImc(imc);
+
+    let dadosUsuariosCompleto = OrganizarDados(dadosUsuario, imc, classificacao);
+
+    CadastrarUsuario(dadosUsuariosCompleto);
+
+    window.location.reload();
+}
+
 function CapturarValores(){
     const nome = document.getElementById('name').value;
-    const altura = document.getElementById('height').value;
-    const peso = document.getElementById('weight').value;
+    const altura = Number(document.getElementById('height').value);
+    const peso = Number(document.getElementById('weight').value);
 
     const dadosUusario = {
         nome: nome,
@@ -13,14 +29,15 @@ function CapturarValores(){
 
 function calcularImc(altura, peso){
     const imc = peso / (altura * altura)
+
     return imc
 }
 
 function ClassificarImc(imc){
     if (imc < 18.5) {
-        return "Abaixo do peso"
+        return "Abaixo do peso!"
     } else if (imc < 25){
-        return "peso normal"
+        return "peso normal!"
     } else if (imc < 30){
         return "sobrepeso"
     } else {
@@ -29,13 +46,70 @@ function ClassificarImc(imc){
 }
 
 function OrganizarDados(dadosUsuario, valorImc, classificacaoImc){
-    const dataHoraAtual = Intl .DateTimeFormat('pt-BR', {timeStyle:'long', dateStyle: 'short'}).format(Date.now());
-    const dadosUsuarioCompleto={
-    ...dadosUsuario;
-    imc:valorImc.toFixed(2),
-    classificacaoImc:classificacaoImc,
-    dataCadastro:dataHoraAtual
+    const dataHoraAtual = Intl.DateTimeFormat('pt-BR', {timeStyle:'long', dateStyle: 'short'}).format(Date.now());
+    const dadosUsuarioCompleto = {
+    ...dadosUsuario,
+    imc: Number(valorImc.toFixed(2)),
+    classificacaoImc: classificacaoImc,
+    dataCadastro: dataHoraAtual
 
     }
+
     return dadosUsuarioCompleto;
 }
+
+function CadastrarUsuario(usuario) {
+    let listaUsuario = [];
+
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuario = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+    }
+
+    listaUsuario.push(usuario)
+
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuario))
+}
+
+function carregarUsuarios() {
+    let listaUsuario = []
+
+    if(localStorage.getItem("usuariosCadastrados")){
+        listaUsuario = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
+
+    if(listaUsuario.length == 0){
+        let tabela = document.getElementById('corpo-tabela');
+
+        tabela.innerHTML = `
+        <tr> 
+            <td colspan="6" class="linha-mensagem"> Nenhum usuário cadastrado </td>
+        </tr>
+        `
+    }
+    else{
+        montarTabela(listaUsuario)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => carregarUsuarios())
+
+function montarTabela(listaDeCadastrados){
+    let tabela = document.getElementById('corpo-tabela')
+
+    let template = " ";
+
+    listaDeCadastrados.forEach(pessoa => {
+        template +=`
+        <tr>
+        <td data-cell="nome" > ${pessoa.nome}</td>
+        <td data-cell="altura" > ${pessoa.altura}</td>
+        <td data-cell="peso" > ${pessoa.peso}</td>
+        <td data-cell="imc" > ${pessoa.imc}</td>
+        <td data-cell="classificacao" > ${pessoa.classificacaoImc}</td>
+        <td data-cell="dataCadastro" > ${pessoa.dataCadastro}</td>
+        </tr>
+        `
+    });
+    tabela.innerHTML = template;
+} 
+
